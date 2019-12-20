5726bedc867ca1d6e4f23a9ff330c9e17113117c
Paul Tondereau <paul.tondereau@yahoo.fr>
2019-12-20 22:00:54 +0100

    splash screen

---
+++ app/src/main/java/com/example/newsapp/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
